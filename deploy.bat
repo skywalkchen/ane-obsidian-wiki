d53892763c659e4113cd50c73f6d2807d62d8bbf
sky_walk <chris11821@gmail.com>
2026-09-09 12:36:57 +0800

    Update wiki: 2026/09/09 週三 12:36:57.13

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 @echo off
-echo 開始上傳最新筆記至 GitHub...
+echo Start uploading to GitHub...
 git add .
 git commit -m "Update wiki: %date% %time%"
 git push origin main
-echo 上傳完成！
+echo MISSION COMPLETED！
 pause
